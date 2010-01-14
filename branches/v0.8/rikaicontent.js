6ff72397c1aad973e8a1084dfcef55fc876c12e6
melink14 <melink14@8ccd9576-f834-11de-aa3c-59de0406b4f5>
2010-01-14 11:40:49 +0000

    Bug fix for issue 26.  rikaikun should now work with webpages that make up their own node names.  At least one trouble area was fixed that is.

--- branches/v0.8/rikaicontent.js
+++ branches/v0.8/rikaicontent.js
@@ -451,7 +451,7 @@ var rcxContent = {
 	},
 
 	isInline: function(node) {
-		return this.inlineNames.hasOwnProperty(node.nodeName) || document.defaultView.getComputedStyle(node,null).getPropertyValue('display') == 'inline';
+		return this.inlineNames.hasOwnProperty(node.nodeName) || ((comStyle = document.defaultView.getComputedStyle(node,null))&& comStyle.getPropertyValue('display') == 'inline');
 	},
 
 	// XPath expression which evaluates to text nodes
@@ -701,10 +701,10 @@ var rcxContent = {
 		var rp = range.startContainer;
 		var ro = range.startOffset;
 		
-/*   		console.log( "offset: " + ro + " parentContainer: " +  rp.nodeName + 
+/*    		console.log( "offset: " + ro + " parentContainer: " +  rp.nodeName + 
 			" total size: " + (rp.data?rp.data.length:"") + " target: " + ev.target.nodeName + 
-			" parentparent: " + rp.parentNode.nodeName); */
-		
+			" parentparent: " + rp.parentNode.nodeName);
+ */		
 
 		
 		if (ev.target == tdata.prevTarget) {
