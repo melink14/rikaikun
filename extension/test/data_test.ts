493a54086300f17eb5f7f763412c994fb4db3098
Erek Speed <melink14@gmail.com>
2022-05-01 11:30:25 +0900

    test(data): Add tests for new logic in data.ts
    - Add chai assertion helpers for loose matching of objects inside of arrays.
    - Add positive and negative cases for new/changed branches.

---
+++ extension/test/data_test.ts
@@ -0,0 +1,62 @@
+import { Config } from '../configuration';
+import { RcxDict } from '../data';
+import { expect, use } from '@esm-bundle/chai';
+import chaiLike from 'chai-like';
+import chaiThings from 'chai-things';
+import sinonChrome from 'sinon-chrome';
+
+// Extend chai-like to allow using regex for fuzzy string matching inside
+// objects.
+chaiLike.extend({
+  match: function (object, expected) {
+    return typeof object === 'string' && expected instanceof RegExp;
+  },
+  assert: function (object, expected) {
+    return expected instanceof RegExp && expected.test(object);
+  },
+});
+
+use(chaiLike);
+use(chaiThings);
+
+let rcxDict: RcxDict;
+
+describe('data.ts', function () {
+  before(async function () {
+    // stub sinon chrome getURL method to return the path it's given
+    // Required to load dictionary files.
+    sinonChrome.extension.getURL.returnsArg(0);
+    rcxDict = await RcxDict.create({} as Config);
+  });
+
+  describe('deinflect', function () {
+    it('should include deinflections of length one or more', function () {
+      expect(rcxDict.deinflect('です')).to.include.something.like({
+        word: 'だ',
+      });
+    });
+
+    it('should not include empty deinflections', function () {
+      expect(rcxDict.deinflect('な')).to.not.include.something.like({
+        word: '',
+      });
+    });
+  });
+
+  describe('wordSearch', function () {
+    it('should return results for deinflected copula', function () {
+      expect(
+        rcxDict.wordSearch('です', /* doNames= */ false)?.data
+      ).to.include.something.like({
+        entry: /^だ .*/,
+        reason: '&lt; polite',
+      });
+    });
+
+    it('should not include copula deinflections for non-copula words', function () {
+      expect(
+        rcxDict.wordSearch('ぼんです', /* doNames= */ false)?.data
+      ).to.not.include.something.like({ entry: /^凡打 .*/ });
+    });
+  });
+});
