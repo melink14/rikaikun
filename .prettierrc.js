482aeb71f150de30bb252e5b25d72f01c4172e1a
Erek Speed <melink14@gmail.com>
2024-08-26 18:09:24 +0900

    build(deps): Bump prettier to >3.0 and prettier-plugin-jsdoc to > 1.0 (#2178)
    - Update prettier config to use ESM module
    - Fix import of prettier-plugin-jsdoc plugin
    - Apply formatting fixes to files due to the new major prettier update.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   // TODO(espeed): Consider reverting bracket spacing to gts default of none.
   bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'es5',
   arrowParens: 'always',
-  plugins: ['./node_modules/prettier-plugin-jsdoc'],
+  plugins: ['./node_modules/prettier-plugin-jsdoc/dist/index.js'],
 };
